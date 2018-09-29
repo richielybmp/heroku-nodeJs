9bfd76dba4d1a2ea9f72098d7e8fcc038ec15ae2
Richiely Paiva <richielybmp@users.noreply.github.com>
2018-09-29 16:14:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const panels =  document.querySelectorAll('.panel');
 function toggleOpen(){   
     panels.forEach(p => p!= this ? p.classList.remove('open') : -1);
     this.classList.toggle('open');
-}
+},
 
 function toggleActive(e){
     if(e.propertyName.includes('flex')){
         this.classList.toggle('open-active');
     }
-}
+},
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
